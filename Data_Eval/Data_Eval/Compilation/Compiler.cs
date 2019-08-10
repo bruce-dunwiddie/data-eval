7feed7137c0e97ad9e82929647ce59a07123ef9a
Bruce Dunwiddie <shriop@hotmail.com>
2019-08-10 03:18:01 -0500

    Updated to use CSharpCodeProvider when running under the Framework or default to Roslyn.

--- Data_Eval/Data_Eval/Compilation/Compiler.cs
+++ Data_Eval/Data_Eval/Compilation/Compiler.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
 
 using Microsoft.CSharp;
 
@@ -12,40 +19,89 @@ namespace Data.Eval.Compilation
 		public Type Compile(
 			string classText)
 		{
-			using (CodeDomProvider provider = new CSharpCodeProvider())
+			string framework = RuntimeInformation.FrameworkDescription;
+
+			// .NET Framework 4.7.3416.0
+			// .NET Core 4.6.26328.01
+			bool isFullFramework = framework.ToUpper().Contains(".NET FRAMEWORK");
+
+			// try to use CSharpCodeProvider from full framework if possible because of faster compile
+			if (isFullFramework)
 			{
-				CompilerParameters options = new CompilerParameters
+				using (CodeDomProvider provider = new CSharpCodeProvider())
 				{
-					CompilerOptions = "/t:library /optimize",
-					GenerateInMemory = true,
-					IncludeDebugInformation = false,
-					TreatWarningsAsErrors = false,
-					WarningLevel = 3,
-					GenerateExecutable = false
-				};
-
-				CompilerResults compileResults = provider.CompileAssemblyFromSource(options, classText);
-
-				if (compileResults.Errors.HasErrors)
-				{
-					string exceptionMessage = "Class failed to compile.";
-
-					foreach (CompilerError error in compileResults.Errors)
+					CompilerParameters options = new CompilerParameters
 					{
-						if (!error.IsWarning)
+						CompilerOptions = "/t:library /optimize",
+						GenerateInMemory = true,
+						IncludeDebugInformation = false,
+						TreatWarningsAsErrors = false,
+						WarningLevel = 3,
+						GenerateExecutable = false
+					};
+
+					CompilerResults compileResults = provider.CompileAssemblyFromSource(options, classText);
+
+					if (compileResults.Errors.HasErrors)
+					{
+						string exceptionMessage = "Class failed to compile.";
+
+						foreach (CompilerError error in compileResults.Errors)
 						{
-							exceptionMessage += "\n\tLine " + error.Line.ToString() + ": " + error.ErrorText;
+							if (!error.IsWarning)
+							{
+								exceptionMessage += "\n\tLine " + error.Line.ToString() + ": " + error.ErrorText;
+							}
 						}
+
+						throw new CompilationException(exceptionMessage)
+						{
+							GeneratedClassCode = classText
+						};
 					}
 
-					throw new CompilationException(exceptionMessage)
-					{
-						GeneratedClassCode = classText
-					};
+					return compileResults.CompiledAssembly.GetType("CustomEvaluator");
 				}
-
-				return compileResults.CompiledAssembly.GetType("CustomEvaluator");
 			}
+			// default to Roslyn
+			else
+			{
+				SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(classText);
+
+				CSharpCompilation compilation = CSharpCompilation.Create(
+					"EvalAssembly",
+					new[] { syntaxTree },
+					new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
+					new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+				using (MemoryStream ms = new MemoryStream())
+				{
+					EmitResult emitResult = compilation.Emit(ms);
+
+					if (!emitResult.Success)
+					{
+						string exceptionMessage = "Class failed to compile.";
+
+						foreach (var error in emitResult.Diagnostics)
+						{							
+							if (error.Severity == DiagnosticSeverity.Error)
+							{								
+								exceptionMessage += "\n\tLine " + error.Location.GetLineSpan().StartLinePosition.Line.ToString() + ": " + error.GetMessage();
+							}
+						}
+
+						throw new CompilationException(exceptionMessage)
+						{
+							GeneratedClassCode = classText
+						};
+					}
+					else
+					{
+						Assembly compiledAssembly = Assembly.Load(ms.ToArray());
+						return compiledAssembly.GetType("CustomEvaluator");
+					}
+				}
+			}			
 		}
 	}
 }
